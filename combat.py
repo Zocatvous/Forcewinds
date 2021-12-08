8f64e615e97e3e9c39437296b51cac93d714ca4e
Jake <jgjedi@gmail.com>
2021-12-08 12:45:20 -0600

    added a new person to the combat script

--- combat.py
+++ combat.py
@@ -2,7 +2,7 @@ import json
 import TurnTracker
 import pprint
 
-x = TurnTracker.TurnTracker([("Voom",15),('Mace Windu',21)])
+x = TurnTracker.TurnTracker([("Someone else",12)("Voom",15),('Mace Windu',21)])
 x.character_list = x.roll_join_combat(verbose=True)
 x.run_combat()
 
